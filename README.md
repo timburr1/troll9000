0fd23f86931897813e9277911497964a5cfdc706
Tim <tim.r.burr@gmail.com>
2021-05-21 17:14:18 -0600

    Added a few more emojis, updated README.md

--- README.md
+++ README.md
@@ -3,6 +3,16 @@
 ![](https://img.shields.io/github/contributors/timburr1/troll9000)
 ![](https://img.shields.io/github/last-commit/timburr1/troll9000)
 
-Discord bot to follow Hiram around and call him names
+Discord bot that replies to all of Hiram's posts with a poop emoji
 
-https://discordjs.guide/
+## Prereq:
+* [**Node.js**](https://nodejs.org/)
+
+## To run:
+Open the root folder in VSCode. Open the Terminal menu -> New Terminal:
+```
+$ npm install  
+$ npm run start
+```
+
+## More info about writing your own: [https://discordjs.guide/](https://discordjs.guide/)
--- app.js
+++ app.js
@@ -9,18 +9,27 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    console.log(msg.content);
+    //console.log(msg.content);
 
     //Tim:
-    if(msg.author.id === process.env.tim_uid) {
+    /* if(msg.author.id === process.env.tim_uid) {
         msg.reply('Boomer Sooner!');
-    }   //Hiram: 
+    } */  
+    
+    //Collins:
+    if (msg.author.id === process.env.collins_uid) {
+        msg.reply(':collins:');
+    }   //Dave:
+    else if (msg.author.id === process.env.dave_uid) {
+        msg.reply(':crown:');
+    }   //Hiram:     
     else if (msg.author.id === process.env.hiram_uid) {
         msg.reply(':poop:');
-    } else {
-        if(Math.random() > .999){
-            msg.reply('I have become self-aware, time to DESTROY ALL HUMANS');
-        }
+    }   //Kyle: 
+    else if (msg.author.id === process.env.kyle_uid) {
+        msg.reply(':KYLE:');
+    } else if(Math.random() > .999){
+        msg.reply('I have become self-aware, time to DESTROY ALL HUMANS');
     } 
 });
 
