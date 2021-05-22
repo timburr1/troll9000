9e72db0d2d7bec3128fd7b7b7b62298bc0eaa634
Tim <tim.r.burr@gmail.com>
2021-05-21 18:42:28 -0600

    Custom emojis are hard :/

--- app.js
+++ app.js
@@ -11,23 +11,13 @@ client.on('ready', () => {
 client.on('message', msg => {
     //console.log(msg.content);
 
-    //Tim:
-    /* if(msg.author.id === process.env.tim_uid) {
-        msg.reply('Boomer Sooner!');
-    } */  
-    
-    //Collins:
-    if (msg.author.id === process.env.collins_uid) {
-        msg.reply(':collins:');
-    }   //Dave:
-    else if (msg.author.id === process.env.dave_uid) {
-        msg.reply(':crown:');
-    }   //Hiram:     
+    //Replying to Tim or Dave:
+    if(msg.author.id === process.env.tim_uid || msg.author.id === process.env.dave_uid) {
+        msg.react('👑');       
+        msg.reply('you are a gentleman and a scholar.');        
+    }    //Hiram:     
     else if (msg.author.id === process.env.hiram_uid) {
-        msg.reply(':poop:');
-    }   //Kyle: 
-    else if (msg.author.id === process.env.kyle_uid) {
-        msg.reply(':KYLE:');
+        msg.react('💩');
     } else if(Math.random() > .999){
         msg.reply('I have become self-aware, time to DESTROY ALL HUMANS');
     } 
